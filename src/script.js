7198ef90f0fa46dd03d4ae906d39125b8cff458b
AndSzy <aszycher@gmail.com>
2021-10-30 22:54:36 +0100

    Fetch Players From API

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
 const myModal = new bootstrap.Modal(document.getElementById('welcome-modal'));
 
 myModal.toggle();
+
+fetch('https://lechia-api.azurewebsites.net/players')
+  .then(response => response.json())
+  .then(players => console.log(players));
