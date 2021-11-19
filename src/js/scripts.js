77ffa7201c28f95dfae390bb31dbed8f2fe92c40
AndSzy <aszycher@gmail.com>
2021-11-19 19:29:00 +0000

    Fixed Height Added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,19 +13,8 @@ fetch('https://lechia-api.azurewebsites.net/videos')
       newVideo.addVideo(postsEl, video.title, convertDate(video.date), video.user_name, video.link);
   })
 
-  const articleEl = document.querySelector('.scroll article')
-  console.log(articleEl.offsetHeight);
-  const scrollEl = document.querySelector('.scroll')
-  const scrollTwitterEl = document.querySelector('.scroll-twitter')
-  let scrollHeight = 2 * articleEl.offsetHeight;
-  scrollEl.style.height = scrollHeight + 'px'
-  scrollTwitterEl.style.height = scrollHeight + 'px'
-
 });
 
-// getting height of element
 
-
-// assigning heigh to scroll
 
 
